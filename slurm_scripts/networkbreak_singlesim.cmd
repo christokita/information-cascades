ed9efa6070bbc60cc9650b642147d4e23aefce06
Chris Tokita <christopher.tokita@gmail.com>
2019-08-19 17:01:50 -0400

    Create networkbreak_singlesim.cmd

---
+++ slurm_scripts/networkbreak_singlesim.cmd
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 20
+#SBATCH -t 2:00:00
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=ctokita@princeton.edu
+
+module load anaconda3
+cd InformationCascades/
+srun python3 scripts/2_Parallel-NetworkBreaking.py
