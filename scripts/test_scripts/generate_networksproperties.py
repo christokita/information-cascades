f78451c34505245aa722d01857e0704002c7290a
Chris Tokita <christopher.tokita@gmail.com>
2019-08-22 14:22:32 -0400

    Run assort network algorithm

--- scripts/test_scripts/generate_networksproperties.py
+++ scripts/test_scripts/generate_networksproperties.py
@@ -12,7 +12,11 @@ Script to test whether assortative/modular networks can be generated
 ####################
 # Load libraryies and packages
 ####################
+import sys
+sys.path.insert(0, '../')
+
 import numpy as np
+import pandas as pd
 import igraph
 from util_scripts.socialnetworkfunctions import *
 from util_scripts.thresholdfunctions import *
@@ -68,14 +72,13 @@ def assortify_network(network, type_vector, assortativity):
     return(adjacency)
 
 ####################
-# Set parameters
+# Test algorithm
 ####################
+##### Set paramters #####
 n = 500 #number of individuals
 k = 4 #mean degree on network
 
-####################
-# Generate 
-####################
+##### Generate network #####
 # Assign type
 type_mat = assign_type(n = n)
 type_vector = type_mat[:,0]
@@ -84,9 +87,7 @@ type_vector = type_mat[:,0]
 adjacency = seed_social_network(n, k)
 adjacency_initial = copy.copy(adjacency)
 
-####################
-# Test algorithm
-####################
+##### Generate assortative network #####
 # Generate assortative network
 adjacency = assortify_network(network = adjacency, type_vector = type_vector, assortativity = 0.9)
 
@@ -98,9 +99,68 @@ g_initial = igraph.Graph.Adjacency(np.ndarray.tolist(adjacency_initial))
 g.vs['Type'] = type_vector
 g_initial.vs['Type'] = type_vector
 
+##### Assess #####
 # Measure assortativity
 final_assort = g.assortativity(types1 = g.vs['Type'])
 initial_assort = g_initial.assortativity(types1 = g_initial.vs['Type'])
 
 print(final_assort)
 print(initial_assort)
+
+'''
+# Measure modularity
+def measure_modularity(graph):
+    # Get community membership
+    dendrogram = graph.community_edge_betweenness()
+    membership = dendrogram.as_clustering().membership
+    # Calculate modularity
+    modularity = graph.modularity(membership)
+    return(modularity)
+    
+measure_modularity(g)
+
+measure_modularity(g_initial)
+'''
+
+####################
+# Run many samples of algorithm
+####################
+##### Set paramters #####
+n = 500 #number of individuals
+k = 4 #mean degree on network
+replicates = 500
+assorts = np.round(np.arange(-1, 1.01, 0.05), 3) #round to deal with weird float values
+
+##### Run algorithm #####
+# Set up results list
+alg_results = pd.DataFrame({'AssortValue' : [], 'ResultAssort' : []})
+
+# Loop through assortativity values
+for assort in assorts:
+    # Print progress message
+    print("Starting Assortatvity = " + str(assort))
+    
+    # Loop through replicates
+    for rep in range(replicates):
+        # Print progress message
+        if (rep/replicates * 100) % 10 == 0:
+            prog = int(rep/replicates * 100)
+            print("..." + str(prog) + "%")
+        
+        # Generate initial network
+        type_mat = assign_type(n = n)
+        type_vector = type_mat[:,0]
+        adjacency = seed_social_network(n, k)
+        adjacency_initial = copy.copy(adjacency)
+        
+        # Run algorithm
+        adj_assort = assortify_network(network = adjacency, type_vector = type_vector, assortativity = assort)
+
+        # Calculate assortativity
+        g = igraph.Graph.Adjacency(np.ndarray.tolist(adjacency))
+        g.vs['Type'] = type_vector
+        final_assort = g.assortativity(types1 = g.vs['Type'])
+        
+        # Return values
+        to_return = pd.Series([assort, final_assort], index = alg_results.columns)
+        alg_results = alg_results.append(to_return, ignore_index=True)
