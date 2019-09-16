cc060db87ac32fce52f2b90c704abb80d12769c2
Chris Tokita <christopher.tokita@gmail.com>
2019-09-16 16:57:58 -0400

    analyze social network data

--- scripts/analyze_socialnetworks.py
+++ scripts/analyze_socialnetworks.py
@@ -33,39 +33,61 @@ list_types = np.load('../output/network_adjust/data/type_data/n200_gamma-0.5.npy
 # Set group size runs of intereset
 n_of_interest = 200
 
-# Find files
-all_files = os.listdir('../output/network_break/data/social_network_data/')
-pars_of_interest = [file for file in all_files if re.findall(str(n_of_interest) + '_gamma[-.0-9]+.npy', file)]
-pars_of_interest = [re.sub('.npy', '', file) for file in pars_of_interest]
+# Directory where simulation data is found
+sn_dir = '../data_sim/network_break/social_network_data/' #social network data
+type_dir = '../data_sim/network_break/type_data/' #type data
+
+# List runs
+runs = os.listdir(sn_dir)
+runs = [run for run in runs if re.findall(str(n_of_interest) + '_gamma[-.0-9]+', run)]
+
 
 ####################
 # Measure assortativity
 ####################
+# Set array for data collection
 assort_values = np.empty((0,2))
 
-for file in pars_of_interest:
+# Loop through runs
+for run in runs:
+    
+    # Load statement
+    print("Starting on \'" + run + "\'...")
     
     # Get gamma value
-    gamma = float(re.search('.*_gamma([-\.0-9]+)', file).group(1))
+    gamma = float(re.search('.*_gamma([-\.0-9]+)', run).group(1))
     
-    # Load network and threshold matrices
-    list_networks = np.load('../output/network_adjust/data/social_network_data/' + file + '.npy')
-    list_types = np.load('../output/network_adjust/data/type_data/' + file + '.npy')
+    # List social network files in that run's data folder
+    sn_files = os.listdir(sn_dir + run +'/')
+    social_networks = [file for file in sn_files if re.findall('sn_rep[0-9]+.npy', file)] #sn data also has initial networks
+    social_networks.sort()
     
-    # Loop through individual replicates  and calculate assortativity
-    for i in np.arange(len(list_networks)):
-        # Get exact network and type matrix
-        adjacency = list_networks[i]
-        type_mat = list_types[i]
+    # List type data files in that run's data folder
+    type_files = os.listdir(type_dir + run +'/')
+    type_files.sort()
+    
+    # Warning and error catch
+    if len(social_networks) != len(type_files):
+        print("The number of replicates do not match in the social network and type data directories.")
+        break
+    
+     # Loop through individual replicates and calculate assortativity
+    for i in np.arange(len(social_networks)):
+        
+        # Load network and threshold matrices
+        adjacency = np.load(sim_dir + run + '/' + social_networks[i])
+        type_mat = np.load(type_dir +  run + '/' + type_files[i])
+        
         # Calculate assortativity
         g = igraph.Graph.Adjacency(np.ndarray.tolist(adjacency))
         g.vs['Type'] = type_mat[:,0]
         final_assort = g.assortativity(types1 = g.vs['Type'])
+        
         # Return
         to_return = np.array([[gamma, final_assort]])
         assort_values = np.vstack([assort_values, to_return])
-        
+            
 # Save
 assort_data= pd.DataFrame(data = assort_values, columns = ['Gamma', 'Assortativity'])
 outfile_name = 'n200_gammasweep_assortativity'
-assort_data.to_csv('../output/network_adjust/data_derived/social_networks/' + outfile_name + '.csv', index = False)
+assort_data.to_csv('../output/network_break/data_derived/social_networks/' + outfile_name + '.csv', index = False)
---
+++ scripts/archive/analyze_socialnetworks.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 10:02:31 2019
+
+@author: ChrisTokita
+
+DESCRIPTION:
+Script to analyze social network structure produced by simulations
+"""
+
+####################
+# Load libraries and packages
+####################
+import numpy as np
+import pandas as pd
+import os
+import re
+import igraph
+
+'''
+####################
+# Load individual data
+####################
+list_networks = np.load('../output/network_adjust/data/social_network_data/n200_gamma-0.5.npy')
+list_networks_initial = np.load('../output/network_adjust/data/social_network_data/n200_gamma-0.5_initial.npy')
+list_types = np.load('../output/network_adjust/data/type_data/n200_gamma-0.5.npy')
+'''
+
+####################
+# List files to be read
+####################
+# Set group size runs of intereset
+n_of_interest = 200
+
+# Find files
+all_files = os.listdir('../output/network_break/data/social_network_data/')
+pars_of_interest = [file for file in all_files if re.findall(str(n_of_interest) + '_gamma[-.0-9]+.npy', file)]
+pars_of_interest = [re.sub('.npy', '', file) for file in pars_of_interest]
+
+####################
+# Measure assortativity
+####################
+assort_values = np.empty((0,2))
+
+for file in pars_of_interest:
+    
+    # Get gamma value
+    gamma = float(re.search('.*_gamma([-\.0-9]+)', file).group(1))
+    
+    # Load network and threshold matrices
+    list_networks = np.load('../output/network_adjust/data/social_network_data/' + file + '.npy')
+    list_types = np.load('../output/network_adjust/data/type_data/' + file + '.npy')
+    
+    # Loop through individual replicates  and calculate assortativity
+    for i in np.arange(len(list_networks)):
+        # Get exact network and type matrix
+        adjacency = list_networks[i]
+        type_mat = list_types[i]
+        # Calculate assortativity
+        g = igraph.Graph.Adjacency(np.ndarray.tolist(adjacency))
+        g.vs['Type'] = type_mat[:,0]
+        final_assort = g.assortativity(types1 = g.vs['Type'])
+        # Return
+        to_return = np.array([[gamma, final_assort]])
+        assort_values = np.vstack([assort_values, to_return])
+        
+# Save
+assort_data= pd.DataFrame(data = assort_values, columns = ['Gamma', 'Assortativity'])
+outfile_name = 'n200_gammasweep_assortativity'
+assort_data.to_csv('../output/network_adjust/data_derived/social_networks/' + outfile_name + '.csv', index = False)
