93b988dbe9c8faaebc072be8dc820387c543fa73
Chris Tokita <christopher.tokita@gmail.com>
2019-10-21 14:03:01 -0400

    fix slurm scripts email setting

--- slurm_scripts/networkbreak_gammas/networkbreak_gamma-0.7.cmd
+++ slurm_scripts/networkbreak_gammas/networkbreak_gamma-0.7.cmd
@@ -3,12 +3,11 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --array=0-99%20
 #SBATCH --time=3:59:00
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
+#SBATCH --mail-type=FAIL
 #SBATCH --mail-user=ctokita@princeton.edu
 #SBATCH --output=slurm_outfiles/slurm-%A_%a.out
 
 ##Load anaconda python packages
 module load anaconda3 
 ##Run script with (1) gamma variable, and (2) replicate number
-srun python3 scripts/2_parallel-networkbreaking.py -0.7 $SLURM_ARRAY_TASK_ID 
+srun python3 scripts/3_hpc-networkbreaking.py -0.7 $SLURM_ARRAY_TASK_ID 
