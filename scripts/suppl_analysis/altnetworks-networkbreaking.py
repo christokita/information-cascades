9715d2a5d29a58fb4a992c81d041eaa6f58d6ad9
Chris Tokita <christopher.tokita@gmail.com>
2019-11-26 17:16:53 -0500

    Update altnetworks-networkbreaking.py

--- scripts/suppl_analysis/altnetworks-networkbreaking.py
+++ scripts/suppl_analysis/altnetworks-networkbreaking.py
@@ -16,8 +16,9 @@ namely the replicate number (taken from a slurm array) and possibly the gamma va
 ####################
 # Load libraries and packages
 ####################
-from model_networkbreaking import sim_adjusting_network
 import sys
+sys.path.append('/home/ctokita/information-cascades/scripts/') # this allows python to search in directory above (i.e., scripts/)
+import model_networkbreaking as model
 
 #NOTE: sys.argv[0] is name of script
 
@@ -32,19 +33,19 @@ p = 0.005 # probability selected individual forms new connection
 timesteps = 100000 #number of rounds simulation will run
 rep = int(sys.argv[2]) #replicate ID number
 
-outpath = '/scratch/gpfs/ctokita/information-cascades/network_break/scale-free/'
+outpath = '/scratch/gpfs/ctokita/information-cascades/network_break/__scale-free/'
 
 
 ##########
 # Run model
 ##########
-sim_adjusting_network(replicate = rep, 
-                        n = n, 
-                        k = k, 
-                        gamma = gamma, 
-                        psi = psi, 
-                        p = p, 
-                        timesteps = timesteps,
-                        outpath = outpath,
-                        networktype = "scalefree")
-
+model. sim_adjusting_network(replicate = rep, 
+                            n = n, 
+                            k = k, 
+                            gamma = gamma, 
+                            psi = psi, 
+                            p = p, 
+                            timesteps = timesteps,
+                            outpath = outpath,
+                            network_type = "scalefree")
+    
