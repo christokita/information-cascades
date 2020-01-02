ac0db0be72b8578d579bb390dfab1a1337fb67a0
Chris Tokita <christopher.tokita@gmail.com>
2020-01-02 11:33:28 -0500

    add scripts to run thresh adjusting model

---
+++ scripts/1_run-threshadjusting.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 12:42:29 2017
+
+@author: ChrisTokita
+
+DESCRIPTION:
+Script to run threshold-adjusting cascade model on local machine
+(single parameter combo, numerous replicates) 
+"""
+import model_threshadjusting as model
+import numpy as np
+
+##########
+# Set parameters
+##########
+n = 200 #number of individuals
+k = 5 #mean degree on networks
+gamma = -0.5 #correlation between two information sources
+psi = 0.1 #proportion of samplers
+phi = 0.01 #amount threhsold changes if individual is correct in behavior
+omega = 0.01 #mount threhsold changes if individual is incorrect in behavior
+timesteps = 100000 #number of rounds simulation will run
+reps = 1 #number of replicate simulations
+
+outpath = '../data_sim/thresh_adjust/'
+
+
+##########
+# Run model
+##########
+for rep in np.arange(reps):
+    model.sim_adjusting_thresholds(replicate = rep,        
+                                    n = n, 
+                                    k = k,  
+                                    gamma = gamma, 
+                                    psi = psi, 
+                                    phi = phi,
+                                    omega = omega,
+                                    timesteps = timesteps,
+                                    outpath = outpath)
+
+
---
+++ scripts/2_parallel-threshadjusting.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 12:42:29 2017
+
+@author: ChrisTokita
+
+DESCRIPTION:
+Script to run threshold-adjusting cascade model on local machine in parallel
+(single parameter combo, numerous replicates) 
+"""
+
+####################
+# Load libraries and packages
+####################
+import model_threshadjusting as model
+import multiprocessing as mp
+import numpy as np
+import sys
+
+#NOTE: sys.argv[0] is name of script
+
+##########
+# Set parameters
+##########
+n = 200 #number of individuals
+k = 5 #mean degree on networks
+gamma = -0.5 #correlation between two information sources
+psi = 0.1 #proportion of samplers
+phi = 0.01 #amount threhsold changes if individual is correct in behavior
+omega = 0.01 #mount threhsold changes if individual is incorrect in behavior
+timesteps = 100000 #number of rounds simulation will run
+reps = 1 #number of replicate simulations
+
+outpath = '../data_sim/thresh_adjust/'
+
+
+##########
+# Run model
+##########
+# Get CPU count and set pool
+cpus = mp.cpu_count()
+pool = mp.Pool(cpus)
+
+# loop through reps
+for rep in np.arange(reps):
+    # Set up iterable parameters for passing to starmap_asyn
+    reps_array = np.arange(reps)
+    n_array = [n] * len(reps_array)
+    k_array = [k] * len(reps_array)
+    gamma_array = [gamma] * len(reps_array)
+    psi_array = [psi] * len(reps_array)
+    phi_array = [phi] * len(reps_array)
+    omega_array = [omega] * len(reps_array)
+    timesteps_array = [timesteps] * len(reps_array)
+    outpath_array = [outpath] * len(reps_array)
+    
+    # Run
+    out = pool.starmap_async(model.sim_adjusting_thresholds, 
+                            zip(reps_array, 
+                                n_array, 
+                                k_array, 
+                                gamma_array, 
+                                psi_array, 
+                                phi_array, 
+                                omega_array,
+                                timesteps_array,
+                                outpath_array))
+
+# Close and join
+pool.close()
+pool.join()
+
+
