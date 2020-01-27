9f54a1024cba0ba5d2c55a88a5bbdd88ccfc77a1
Chris Tokita <christopher.tokita@gmail.com>
2020-01-27 14:20:47 -0500

    Plot tie breaks and adds by ideology

--- scripts/plot_socialnetstructure.R
+++ scripts/plot_socialnetstructure.R
@@ -123,7 +123,8 @@ network_change_data <- do.call("rbind", network_change_data)
 ##########
 # Change in connections by type
 net_type_data <- network_change_data %>% 
-  filter(metric %in% c("net_same", "net_diff"))
+  filter(metric %in% c("net_same", "net_diff")) %>% 
+  mutate(metric = factor(metric, levels = c("net_same", "net_diff")))
 gg_type_change <- ggplot(net_type_data, aes(x = gamma, y = mean)) +
   geom_hline(yintercept = 0, 
              size = 0.3, 
@@ -135,13 +136,13 @@ gg_type_change <- ggplot(net_type_data, aes(x = gamma, y = mean)) +
              size = 0.8) +
   ylab(expression( paste(Delta, " social ties")) ) +
   xlab(expression( paste("Information correlation ", italic(gamma)) )) +
-  scale_shape_manual(values = c(21, 19),
-                     labels = c("Diff. ideology",
-                                "Same ideology"),
+  scale_shape_manual(values = c(19, 21),
+                     labels = c("Same ideology",
+                                "Diff. ideology"),
                      name = "Connetion type") +
-  scale_fill_manual(values = c("white", "black"),
-                    labels = c("Diff. ideology",
-                               "Same ideology"),
+  scale_fill_manual(values = c("black", "white"),
+                    labels = c("Same ideology",
+                               "Diff. ideology"),
                     name = "Connetion type") +
   theme_ctokita() +
   theme(aspect.ratio = 1)
@@ -155,6 +156,45 @@ ggsave(plot = gg_type_change,
        units = "mm", 
        dpi = 400)
 
+
+# Breaks/new ties by gamma
+ties_data <- network_change_data %>% 
+  filter(metric %in% c("diff_type_adds", "diff_type_breaks", "same_type_adds", "same_type_breaks")) %>% 
+  mutate(metric = factor(metric, levels = c("same_type_adds", "same_type_breaks", "diff_type_adds", "diff_type_breaks")))
+gg_ties <- ggplot(ties_data, aes(x = gamma, y = mean, group = metric)) +
+  geom_line(size = 0.3) +
+  geom_errorbar(aes(ymax = mean + error, ymin = mean - error),
+                size = 0.3,
+                width = 0) +
+  geom_point(aes(shape = metric, fill = metric),
+             size = 0.8) +
+  ylab("Count") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  scale_y_continuous(limits = c(0, 1.21), 
+                     breaks = seq(0, 2, 0.2),
+                     expand = c(0, 0)) +
+  scale_shape_manual(values = c(19, 17, 21, 24),
+                     labels = c("New tie, same ideology",
+                                "Broken tie, same ideology",
+                                "New tie, diff. ideology",
+                                "Broken tie, diff. ideology"),
+                     name = "") +
+  scale_fill_manual(values = c("black", "black", "white", "white"),
+                    labels = c("New tie, same ideology",
+                               "Broken tie, same ideology",
+                               "New tie, diff. ideology",
+                               "Broken tie, diff. ideology"),
+                    name = "") +
+  theme_ctokita() +
+  theme(aspect.ratio = 1)
+gg_ties
+
+ggsave(plot = gg_ties, 
+       filename = "output/network_break/social_networks/tie_breaksandadds_gamma.png", 
+       width = 90, 
+       height = 45, 
+       units = "mm", 
+       dpi = 400)
 
 # Change in degree
 net_degree_data <- network_change_data %>% 
