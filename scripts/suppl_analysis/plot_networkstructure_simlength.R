eaf427da570cd5f1d74aa102c86ea7bd6fde8782
Chris Tokita <christopher.tokita@gmail.com>
2020-01-29 14:34:35 -0500

    Sim length and network structure change plots

--- scripts/suppl_analysis/plot_networkstructure_simlength.R
+++ scripts/suppl_analysis/plot_networkstructure_simlength.R
@@ -99,4 +99,162 @@ ggsave(plot = gg_assort_simlength,
        width = 90, units = "mm", dpi = 400)
 
 
-############## Network change ##############
+############## Changes in network structure ##############
+
+##########
+# Load data and summarise
+##########
+# Normal sim length (10^5)
+normal_files <- list.files("data_derived/network_break/social_networks/network_change/", full.names = TRUE)
+norm_data <- lapply(normal_files, function(x) {
+  # Read in file 
+  run_file <- read.csv(x)
+  #Summarize
+  run_data <- run_file %>% 
+    mutate(net_same = same_type_adds - same_type_breaks,
+           net_diff = diff_type_adds - diff_type_breaks,
+           net_out_degree = out_degree - out_degree_initial,
+           net_in_degree = in_degree - in_degree_initial,
+           sim_length = "10^5") %>% 
+    select(-replicate, -individual) %>% 
+    gather(metric, value, -gamma, -sim_length) %>% 
+    group_by(sim_length, gamma, metric) %>% 
+    summarise(mean = mean(value), 
+              sd = sd(value),
+              error = sd(value)/sqrt(length(value)))
+  return(run_data)
+})
+norm_data <- do.call("rbind", norm_data)
+
+# Long sim length (10^6)
+long_files <- list.files("data_derived/network_break/__suppl_analysis/sim_length/network_change/", full.names = TRUE)
+long_data <- lapply(long_files, function(x) {
+  # Read in file 
+  run_file <- read.csv(x)
+  #Summarize
+  run_data <- run_file %>% 
+    mutate(net_same = same_type_adds - same_type_breaks,
+           net_diff = diff_type_adds - diff_type_breaks,
+           net_out_degree = out_degree - out_degree_initial,
+           net_in_degree = in_degree - in_degree_initial,
+           sim_length = "10^6") %>% 
+    select(-replicate, -individual) %>% 
+    gather(metric, value, -gamma, -sim_length) %>% 
+    group_by(sim_length, gamma, metric) %>% 
+    summarise(mean = mean(value), 
+              sd = sd(value),
+              error = sd(value)/sqrt(length(value)))
+  return(run_data)
+})
+long_data <- do.call("rbind", long_data)
+
+# Bind
+network_change_data <- rbind(norm_data, long_data)
+
+##########
+# Plot
+##########
+# Change in connections by type
+net_type_data <- network_change_data %>% 
+  filter(metric %in% c("net_same", "net_diff")) %>% 
+  mutate(metric = factor(metric, levels = c("net_same", "net_diff")))
+gg_type_change <- ggplot(net_type_data, aes(x = gamma, y = mean, color = sim_length)) +
+  geom_hline(yintercept = 0, 
+             size = 0.3, 
+             linetype = "dotted") +
+  geom_errorbar(aes(ymax = mean + error, ymin = mean - error),
+                size = 0.3,
+                width = 0) +
+  geom_point(aes(shape = metric, fill = metric),
+             size = 0.8) +
+  ylab(expression( paste(Delta, " social ties")) ) +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  scale_shape_manual(values = c(19, 21),
+                     labels = c("Same ideology",
+                                "Diff. ideology"),
+                     name = "Connetion type") +
+  scale_fill_manual(values = c("black", "white"),
+                    labels = c("Same ideology",
+                               "Diff. ideology"),
+                    name = "Connetion type") +
+  theme_ctokita() +
+  theme(aspect.ratio = 1)
+
+gg_type_change
+
+ggsave(plot = gg_type_change, 
+       filename = "output/network_break/social_networks/tiechange_gamma.png", 
+       width = 75, 
+       height = 45, 
+       units = "mm", 
+       dpi = 400)
+
+
+# Breaks/new ties by gamma
+ties_data <- network_change_data %>% 
+  filter(metric %in% c("diff_type_adds", "diff_type_breaks", "same_type_adds", "same_type_breaks")) %>% 
+  mutate(set = paste0(sim_length, "-", metric),
+         metric = factor(metric, levels = c("same_type_adds", "same_type_breaks", "diff_type_adds", "diff_type_breaks")))
+gg_ties <- ggplot(ties_data, aes(x = gamma, y = mean, group = metric, color = sim_length)) +
+  geom_line(aes(group = set),
+            size = 0.3) +
+  geom_errorbar(aes(ymax = mean + error, ymin = mean - error),
+                size = 0.3,
+                width = 0) +
+  geom_point(aes(shape = metric, fill = metric),
+             size = 0.8) +
+  ylab("Count") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  scale_shape_manual(values = c(19, 17, 21, 24),
+                     labels = c("New tie, same ideology",
+                                "Broken tie, same ideology",
+                                "New tie, diff. ideology",
+                                "Broken tie, diff. ideology"),
+                     name = "") +
+  scale_fill_manual(values = c("black", "black", "white", "white"),
+                    labels = c("New tie, same ideology",
+                               "Broken tie, same ideology",
+                               "New tie, diff. ideology",
+                               "Broken tie, diff. ideology"),
+                    name = "") +
+  theme_ctokita() +
+  theme(aspect.ratio = 1)
+gg_ties
+
+ggsave(plot = gg_ties, 
+       filename = "output/network_break/suppl_analysis/Breaksandadds_simlength.png", 
+       width = 90, 
+       height = 45, 
+       units = "mm", 
+       dpi = 400)
+
+gg_ties <- gg_ties +
+  scale_y_continuous(limits = c(0, 1.5))
+
+ggsave(plot = gg_ties, 
+       filename = "output/network_break/suppl_analysis/Breaksandadds_simlength_zoom.png", 
+       width = 90, 
+       height = 45, 
+       units = "mm", 
+       dpi = 400)
+
+# Change in degree
+net_degree_data <- network_change_data %>% 
+  filter(metric %in% c("net_out_degree")) %>% 
+  mutate(log_mean = log10(mean))
+gg_degree_change <- ggplot(net_degree_data, aes(x = gamma, y = mean, color = sim_length)) +
+  geom_errorbar(aes(ymax = mean + error, ymin = mean - error),
+                size = 0.3,
+                width = 0) +
+  geom_point(aes(shape = metric, fill = metric),
+             size = 0.8) +
+  ylab(expression( paste(Delta, " out-degree"))) +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  # scale_y_continuous(limits = c(0, 1.5), 
+  #                    expand = c(0, 0)) +
+  theme_ctokita() +
+  theme(aspect.ratio = 1,
+        legend.position = "none")
+
+gg_degree_change
+
