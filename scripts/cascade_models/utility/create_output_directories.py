e64851d4340844c31ca4a22b368bfb3e9ba4b6c9
Chris Tokita <christopher.tokita@gmail.com>
2020-02-12 16:23:12 -0500

    Update create_output_directories.py

--- scripts/cascade_models/utility/create_output_directories.py
+++ scripts/cascade_models/utility/create_output_directories.py
@@ -5,8 +5,10 @@ Created on Wed Feb 12 15:17:06 2020
 
 @author: ChrisTokita
 """
+import numpy as np
+import os
 
-def create_output_directories(outpath, n, gamma):
+def create_output_directories(outpath, directory_names, n, gamma):
     # Creates the output directories where simulation data will be stored. 
     #
     # INPUTS:
@@ -18,14 +20,14 @@ def create_output_directories(outpath, n, gamma):
     # - output_dirs:   list of full paths to the output directories that were created.
     
     output_name = "n" + str(n) + "_gamma" + str(gamma)
-    data_dirs = ['cascade_data', 'social_network_data', 'thresh_data', 'type_data', 'behavior_data', 'fitness_data']
-    data_dirs = [outpath + d + "/" for d in data_dirs]
-    output_dirs = [d + output_name +  "/" for d in data_dirs]
-    for x in np.arange(len(data_dirs)):
+    directory_names = ['cascade_data', 'social_network_data', 'thresh_data', 'type_data', 'behavior_data', 'fitness_data']
+    data_directories = [outpath + d + "/" for d in directory_names]
+    output_directories = [d + output_name +  "/" for d in data_directories]
+    for x in np.arange(len(data_directories)):
         # Check if directory already exisits. If not, create it.
-        if not os.path.exists(data_dirs[x]):
-            os.makedirs(data_dirs[x])
+        if not os.path.exists(data_directories[x]):
+            os.makedirs(data_directories[x])
         # Check if specific run folder exists
-        if not os.path.exists(output_dirs[x]):
-            os.makedirs(output_dirs[x])
-    return output_dirs
+        if not os.path.exists(output_directories[x]):
+            os.makedirs(output_directories[x])
+    return output_directories
