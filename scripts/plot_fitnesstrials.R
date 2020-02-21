08ecf2ea109d85de39b59828285ac9ea06a1a39e
Chris Tokita <christopher.tokita@gmail.com>
2020-02-21 10:43:22 -0500

    combine plot scripts for fitness trial data

---
+++ scripts/plot_fitnesstrials.R
@@ -0,0 +1,175 @@
+########################################
+#
+# PLOT: Results of fitness trials both both cascade dyanmics and individual behavior/information use
+#
+########################################
+
+####################
+# Load packages
+####################
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+source("scripts/_plot_themes/theme_ctokita.R")
+
+####################
+# Paramters for analysis: paths to data, paths for output, and filename
+####################
+fit_cascade_path <- 'data_derived/network_break/fitness_trials/fitness_cascadestats_gammasweep.csv' #path to fitness cascade data
+fit_behavior_path <- "data_derived/network_break/fitness_trials/fitness_behavior_gammasweep.csv" #path to fitness behavior data
+out_path <- "output/network_break/fitness_trials/" #directory you wish to save plots
+plot_tag <- "gammasweep" #extra info to add onto end of plot name
+if (plot_tag != "") {
+  plot_tag <- paste0("_", plot_tag)
+}
+
+
+
+############################## Ftiness trials: Cascade dynamics ##############################
+
+####################
+# Load data 
+####################
+# Read in data
+cascade_data <- read.csv(fit_cascade_path, header = TRUE)
+
+####################
+# Plot: Cascade size
+####################
+# Summarise by gamma
+cascade_size <- cascade_data %>% 
+  select(gamma, total_active) %>% 
+  gather(metric, value, -gamma) %>% 
+  group_by(gamma, metric) %>% 
+  summarise(mean = mean(value, na.rm = TRUE),
+            sd = sd(value, na.rm = TRUE),
+            ci95 = qnorm(0.975) * sd(value, na.rm = TRUE) / sqrt( sum(!is.na(value)) )) #denominator removes NA values from count
+
+# Plot
+gg_size <- ggplot(cascade_size, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95, 
+                  ymax = mean + ci95), 
+              alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  ylab("Cascade size") +
+  xlab(expression(paste("Information correlation ", italic(gamma) ))) +
+  theme_ctokita() 
+gg_size #show plot before saving
+ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".png"), width = 45, height = 45, units = "mm", dpi = 400)
+ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".svg"), width = 45, height = 45, units = "mm")
+
+####################
+# Plot: Cascade bias
+####################
+# Summarise by gamma
+cascade_bias <- cascade_data %>% 
+  select(gamma, cascade_bias) %>% 
+  gather(metric, value, -gamma) %>% 
+  group_by(gamma, metric) %>% 
+  summarise(mean = mean(value),
+            sd = sd(value),
+            ci95 = qnorm(0.975) * sd(value, na.rm = TRUE) / sqrt( sum(!is.na(value)) ))
+
+# Summarizing plot
+gg_bias <- ggplot(cascade_bias, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95, 
+                  ymax = mean + ci95),
+              alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  # scale_y_continuous(limits = c(0, 0.16)) +
+  ylab(expression( paste("Cascade bias" ))) +
+  xlab(expression(paste("Information correlation ", italic(gamma) ))) +
+  theme_ctokita() 
+gg_bias #show plot before saving
+ggsave(plot = gg_bias, filename = paste0(out_path, "cascadebias", plot_tag,".png"), width = 45, height = 45, units = "mm", dpi = 400)
+ggsave(plot = gg_bias, filename = paste0(out_path, "cascadebias", plot_tag,".svg"), width = 45, height = 45, units = "mm")
+
+
+
+
+############################## Fitness trials: individual behavior/information use ##############################
+
+####################
+# Load data and summarise
+####################
+behav_data <- read.csv(fit_behavior_path, header = TRUE)
+behav_sum <- behav_data %>% 
+  select(-replicate) %>% 
+  mutate(fitness = sensitivity + specificity + precision) %>% 
+  gather(metric, value, -gamma, -threshold) %>% 
+  group_by(gamma, metric) %>% 
+  summarise(mean = mean(value, na.rm = TRUE),
+            sd = sd(value, na.rm = TRUE),
+            ci95 = qnorm(0.975) * sd(value, na.rm = TRUE) / sqrt( sum(!is.na(value)) )) #denominator removes NA values from count
+
+####################
+# Plot
+####################
+# Sensitivity: proportion of important (i.e., greater than threshold) news stories individual reacted to
+sensitivity_data <- behav_sum %>% 
+  filter(metric == "sensitivity")
+gg_sens <- ggplot(data = sensitivity_data, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95,
+                  ymax =  mean + ci95),
+                  alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  ylab("Behavioral sensitivity") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  theme_ctokita() 
+gg_sens #show plot before saving
+ggsave(plot = gg_sens, filename = paste0(out_path, "sensitvity", plot_tag, ".png"), width = 45, height = 45, units = "mm", dpi = 600)
+ggsave(plot = gg_sens, filename = paste0(out_path, "sensitvity", plot_tag, ".svg"), width = 45, height = 45, units = "mm")
+
+
+# Specificity: proportion of "unimportant" (i.e, less than threshold) stories an individual did *not* react to
+specificity_data <- behav_sum %>% 
+  filter(metric == "specificity")
+gg_specif <- ggplot(data = specificity_data, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95,
+                  ymax =  mean + ci95),
+              alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  ylab("Behavioral specificity") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  theme_ctokita() 
+gg_specif #show plot before saving
+ggsave(plot = gg_specif, filename = paste0(out_path, "specificity", plot_tag, ".png"), width = 45, height = 45, units = "mm", dpi = 600)
+ggsave(plot = gg_specif, filename = paste0(out_path, "specificity", plot_tag, ".svg"), width = 45, height = 45, units = "mm")
+
+
+# Precision: proportion of activity (x_i = 1) that is due to "important" news.
+precision_data <- behav_sum %>% 
+  filter(metric == "precision")
+gg_precis <- ggplot(data = precision_data, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95,
+                  ymax =  mean + ci95),
+              alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  ylab("Behavioral precision") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  theme_ctokita() 
+gg_precis #show plot before saving
+ggsave(plot = gg_precis, filename = paste0(out_path, "precision", plot_tag, ".png"), width = 45, height = 45, units = "mm", dpi = 600)
+ggsave(plot = gg_precis, filename = paste0(out_path, "precision", plot_tag, ".svg"), width = 45, height = 45, units = "mm")
+
+
+# Individual fitness 
+fitness_data <- behav_sum %>% 
+  filter(metric == "fitness")
+gg_fitness <- ggplot(data = fitness_data, aes(x = gamma, y = mean)) +
+  geom_ribbon(aes(ymin = mean - ci95,
+                  ymax =  mean + ci95),
+              alpha = 0.4) +
+  geom_line(size = 0.3) +
+  geom_point(size = 0.8) +
+  ylab("Information use fitness") +
+  xlab(expression( paste("Information correlation ", italic(gamma)) )) +
+  theme_ctokita() 
+gg_fitness #show plot before saving
+ggsave(plot = gg_fitness, filename = paste0(out_path, "fitness", plot_tag, ".png"), width = 45, height = 45, units = "mm", dpi = 600)
+
