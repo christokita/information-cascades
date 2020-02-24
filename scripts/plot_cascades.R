3c5d0f651f6c92609b82b56ed2562af50c4f782a
Chris Tokita <christopher.tokita@gmail.com>
2020-02-24 11:27:21 -0500

    Plot cascade data

--- scripts/plot_cascades.R
+++ scripts/plot_cascades.R
@@ -52,7 +52,7 @@ ggplot(data = sample_data, aes(x = time, y = value, color = gamma, group = repli
   theme_ctokita() 
 
 ####################
-# Plot: Cascade size difference over simulation
+# Plot: Cascade size at the beginning and end of the simulation
 ####################
 # Filter
 beginend_size <- beginend_sum %>% 
@@ -70,12 +70,39 @@ gg_beginend_size <- ggplot(beginend_size, aes(x = time, y = mean, color = gamma,
   geom_line(size = 0.3, alpha = 0.8) +
   geom_point(size = 0.8) +
   scale_color_gradientn(colors = pal) +
+  scale_x_discrete(labels = c("First 5,000", "Last 5,000")) +
   ylab("Cascade size") +
-  xlab("Simulation time") +
-  theme_ctokita() 
+  xlab("Time steps") +
+  theme_ctokita()
 gg_beginend_size #show plot before saving
-ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".png"), width = 45, height = 45, units = "mm", dpi = 400)
-ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".svg"), width = 45, height = 45, units = "mm")
+ggsave(plot = gg_beginend_size, filename = paste0(out_path, "beginend_size", plot_tag ,".png"), width = 75, height = 45, units = "mm", dpi = 400)
+# ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".svg"), width = 45, height = 45, units = "mm")
+
+####################
+# Plot: Cascade bias at beginning and end of cascdae
+####################
+# Filter
+beginend_bias <- beginend_sum %>% 
+  filter(metric %in% c("bias_begin", "bias_end")) %>% 
+  mutate(time = factor(metric))
+
+# Plot
+pal <- brewer.pal(6, "PuOr")
+gg_beginend_bias <- ggplot(beginend_bias, aes(x = time, y = mean, color = gamma, group = gamma)) +
+  # geom_ribbon(aes(ymin = mean - sd,
+  #                 ymax = mean + sd,
+  #                 fill = gamma),
+  #                 alpha = 0.1,
+  #             color = NA) +
+  geom_line(size = 0.3, alpha = 0.8) +
+  geom_point(size = 0.8) +
+  scale_color_gradientn(colors = pal) +
+  scale_x_discrete(labels = c("First 5,000", "Last 5,000")) +
+  ylab("Cascade bias") +
+  xlab("Time steps") +
+  theme_ctokita() 
+gg_beginend_bias #show plot before saving
+ggsave(plot = gg_beginend_bias, filename = paste0(out_path, "beginend_bias", plot_tag ,".png"), width = 75, height = 45, units = "mm", dpi = 400)
 
 
 
@@ -88,9 +115,16 @@ ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".sv
 avgcasc_data <- read.csv(cascadesum_path, header = TRUE) %>% 
   tidyr::gather("metric", "value", -gamma, -t)
 avgcasc_sum <- avgcasc_data %>% 
-  group_by(gamma, metric) %>% 
+  group_by(t, gamma, metric) %>% 
   summarise(mean = mean(value),
             sd = sd(value),
             ci95 = qnorm(0.975)*sd(value)/sqrt( sum(!is.na(value)) ))
 
-
+####################
+# Plot: Cascade size over course of simulation
+####################
+test <- avgcasc_data %>% 
+  filter(gamma == 0.5, metric == "cascade_bias")
+gg_sim_size <- ggplot(data = test, aes(x = t, y = value)) +
+  geom_line()
+gg_sim_size
