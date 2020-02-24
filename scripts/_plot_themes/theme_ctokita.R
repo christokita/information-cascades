bbbf439f3d0699d1bdd948c2a347f7ca1da9cd8d
Chris Tokita <christopher.tokita@gmail.com>
2020-02-24 10:59:12 -0500

    Update theme_ctokita.R

--- scripts/_plot_themes/theme_ctokita.R
+++ scripts/_plot_themes/theme_ctokita.R
@@ -19,6 +19,7 @@ theme_ctokita <- function() {
           legend.text     = element_text(size = 6, color = "black"),
           strip.text      = element_text(size = 7, color = "black"),
           legend.key.size = unit(3, "mm"),
+          legend.key.width = unit(2, "mm"),
           aspect.ratio    = 1)
 }
 
