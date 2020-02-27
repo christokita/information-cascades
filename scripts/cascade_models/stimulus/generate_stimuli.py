6f37deb864b099cfd301d70b8df408396a2282f5
Chris Tokita <christopher.tokita@gmail.com>
2020-02-27 12:08:12 -0500

    Clean up main generate_stimuli script

--- scripts/cascade_models/stimulus/generate_stimuli.py
+++ scripts/cascade_models/stimulus/generate_stimuli.py
@@ -19,19 +19,3 @@ def generate_stimuli(correlation, mean):
     
     stims_sig = st.generate_stimuli_cdf(correlation, mean)
     return stims_sig
-
-
-#def generate_stimuli(correlation, mean):
-#    # Generates a single pair of stimuli/infromation values for the two news sources.
-#    # Values are rescaled to the range [0, 1] using a logistic function.
-#    # ** This is currently the method used in the model. **
-#    #
-#    # INPUTS:
-#    # - correlation:   the correlation between the two information sources during random samples (float).
-#    # - mean:          average out-degree desired in social network (float or int).
-#    
-#    covar = [[1, correlation ], [correlation, 1]] 
-#    stims = np.random.multivariate_normal(mean = [mean, mean], cov = covar, size = 1) 
-#    stims_sig = 1 / (1 + np.exp(-stims+mean)) # Translate stims to 0 to 1 scale
-#    return stims_sig
-    
