89416a2fddbc8906e4415750220d77d1066c228c
Chris Tokita <christopher.tokita@gmail.com>
2020-03-03 19:22:46 -0500

    Plot example network of identical thresh p0 long sim

--- scripts/process_getexamplenetworks.py
+++ scripts/process_getexamplenetworks.py
@@ -22,52 +22,60 @@ import copy
 ####################
 # Parameters of files to be read
 ####################
-# Set group size runs of intereset
-n_of_interest = 200
-
 # Directory where simulation data is found
-sn_dir = '../data_sim/network_break/social_network_data/' #social network data
-type_dir = '../data_sim/network_break/type_data/' #type data
-thresh_dir = '../data_sim/network_break/thresh_data/' #threshold data
+sn_dir = '../data_sim/network_break/__suppl_sims/identical_thresholds_p0_longsim/social_network_data/' #social network data
+type_dir = '../data_sim/network_break/__suppl_sims/identical_thresholds_p0_longsim/type_data/' #type data
+thresh_dir = '../data_sim/network_break/__suppl_sims/identical_thresholds_p0_longsim/thresh_data/' #threshold data
 
+# For output
+outpath = '../data_derived/network_break/__suppl_analysis/identical_thresholds_p0_longsim/social_networks/'
+filetags = 'identicalthresh_p0_10^6' #added info for save file
+if len(filetags) > 0:
+    filetags = '_' + filetags
 
 ####################
 # Output example social network for plotting in Gephi
 ####################
 # Read in data
-outfile_name = 'n' + str(n_of_interest) + '_gammasweep_assortativity'
-assort_data = pd.read_csv('../data_derived/network_break/social_networks/' + outfile_name + '.csv')
+assort_file = [file for file in os.listdir(outpath) if re.findall('assortativity.*.csv', file)]
+if len(assort_file) > 1:
+    print("WARNING: more than one file matched search pattern for assortativity data.")
+    assort_data = None
+else:
+    assort_data = pd.read_csv(outpath + assort_file[0])
 
 # Find high assortatiity graph
-high_assort = assort_data[assort_data.Assortativity > 0.26] #grab high assort values
-high_assort = high_assort[high_assort.Gamma.isin([-1, -0.9])] #grab gammas of interest
-high_assort_ind = high_assort.Assortativity.idxmax() #get index of max value
+assort_percentile = np.percentile(assort_data.assort_final, 95)
+high_assort = assort_data[assort_data.assort_final > assort_percentile] #grab high assort values
+high_assort = high_assort[high_assort.gamma.isin([-1, -0.9])] #grab gammas of interest
+high_assort_ind = high_assort.assort_final.idxmax() #get index of max value
 
 # Grab corresponding graph 
-high_assort_gamma = assort_data.Gamma[high_assort_ind] #get gamma value
-begin_this_gamma = min(assort_data.index[assort_data.Gamma == high_assort_gamma]) #find where this gamma starts
+high_assort_gamma = assort_data.gamma[high_assort_ind] #get gamma value
+begin_this_gamma = min(assort_data.index[assort_data.gamma == high_assort_gamma]) #find where this gamma starts
 high_assort_replicate = high_assort_ind - begin_this_gamma + 1 #determine replicate number of that gamma value
-sn_path = sn_dir + 'n' + str(n_of_interest) + '_gamma' + str(high_assort_gamma) + '/'
-sn_name = 'sn_rep' + str(high_assort_replicate).zfill(2) + '.npy'
+sn_path = sn_dir + 'gamma' + str(high_assort_gamma) + '/'
+sn_name = 'sn_final_rep' + str(high_assort_replicate).zfill(2) + '.npy'
 high_assort_graph = np.load(sn_path + sn_name) #load
 
 # Grab corresponding type data
-type_path = type_dir + 'n' + str(n_of_interest) + '_gamma' + str(high_assort_gamma) + '/'
+type_path = type_dir + 'gamma' + str(high_assort_gamma) + '/'
 type_name = 'type_rep' + str(high_assort_replicate).zfill(2) + '.npy'
 high_assort_types = np.load(type_path + type_name) #load
 
 # Grab corresponding threshold data  
-thresh_path = thresh_dir + 'n' + str(n_of_interest) + '_gamma' + str(high_assort_gamma) + '/'
+thresh_path = thresh_dir + 'gamma' + str(high_assort_gamma) + '/'
 thresh_name = 'thresh_rep' + str(high_assort_replicate).zfill(2) + '.npy'
 high_assort_thresh = np.load(thresh_path + thresh_name) #load
 
 # Prepre for gephi and save
+n_individuals = high_assort_graph.shape[0]
 high_assort_graph = pd.DataFrame(high_assort_graph, 
-                                 columns = np.arange(0, n_of_interest), 
-                                 index = np.arange(n_of_interest))
-high_assort_nodes = pd.DataFrame({"Id": np.arange(0, n_of_interest), 
+                                 columns = np.arange(0, n_individuals), 
+                                 index = np.arange(n_individuals))
+high_assort_nodes = pd.DataFrame({"Id": np.arange(0, n_individuals), 
                                   "Type": high_assort_types[:,0],
                                   "Threshold": high_assort_thresh[:,0]})
 filename = "examplenet_highassort"
-high_assort_graph.to_csv('../data_derived/network_break/social_networks/' + filename + ".csv")
-high_assort_nodes.to_csv('../data_derived/network_break/social_networks/' + filename + "_nodes.csv", index = False)
+high_assort_graph.to_csv(outpath + "examplenet_network" + filetags + ".csv")
+high_assort_nodes.to_csv(outpath + "examplenet_nodes" + filetags + ".csv", index = False)
