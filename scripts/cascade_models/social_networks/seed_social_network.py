6f78ae2d26263f5dea570208ccfaf20ad2f5ce75
Chris Tokita <christopher.tokita@gmail.com>
2020-04-06 10:18:23 -0400

    Turn off prevent loner functionality in initial seeding

--- scripts/cascade_models/social_networks/seed_social_network.py
+++ scripts/cascade_models/social_networks/seed_social_network.py
@@ -31,28 +31,29 @@ def seed_social_network(n, k, network_type, directed = False):
     network = g.get_adjacency()
     network = np.array(network.data)
     
-    # Prevent loners
-    if sum( np.sum(network, axis = 1) == 0 ) > 0:
-        
-        # Find loners
-        loners = np.where(np.sum(network, axis = 1) == 0)[0]
-        number_of_breaks = len(loners) #numver of links that need to broken
-        
-        # Find and break links to keep mean degree the same
-        two_or_more = np.where(np.sum(network, axis = 1) > 1)[0]
-        break_links = np.random.choice(two_or_more, replace = False, size = number_of_breaks)
-        for break_link in break_links:
-            # Choose link at random and break/zero out
-            connections = np.where(network[break_link,] == 1)[0]
-            break_this = np.random.choice(connections, size = 1)
-            network[break_link, break_this] = 0
-            
-        # Add random links to loners
-        for loner in loners:
-            possible_connections = np.arange(0, n)
-            possible_connections = np.delete(possible_connections, loner)
-            new_connection = np.random.choice(possible_connections, size = 1)
-            network[loner, new_connection] = 1
+#    # Prevent loners
+#    if sum( np.sum(network, axis = 1) == 0 ) > 0:
+#        
+#        # Find loners
+#        loners = np.where(np.sum(network, axis = 1) == 0)[0]
+#        number_of_breaks = len(loners) #numver of links that need to broken
+#        
+#        # Find and break links to keep mean degree the same
+#        two_or_more = np.where(np.sum(network, axis = 1) > 1)[0]
+#        break_links = np.random.choice(two_or_more, replace = False, size = number_of_breaks)
+#        for break_link in break_links:
+#            # Choose link at random and break/zero out
+#            connections = np.where(network[break_link,] == 1)[0]
+#            break_this = np.random.choice(connections, size = 1)
+#            network[break_link, break_this] = 0
+#            network[break_this, break_link] = 0
+#            
+#        # Add random links to loners
+#        for loner in loners:
+#            possible_connections = np.arange(0, n)
+#            possible_connections = np.delete(possible_connections, loner)
+#            new_connection = np.random.choice(possible_connections, size = 1)
+#            network[loner, new_connection] = 1
             
     # Return
     return network
