f2240aa3dc2b3947985e01f5a2ee31d0aaa3585d
Chris Tokita <christopher.tokita@gmail.com>
2020-04-07 08:33:00 -0400

    Give assess_fitess it's own evaluate_behavior function

--- scripts/cascade_models/cascades/assess_fitness.py
+++ scripts/cascade_models/cascades/assess_fitness.py
@@ -49,11 +49,43 @@ def assess_fitness(n, gamma, psi, trial_count, network, thresholds, types):
                                              types = types, 
                                              stats_df = cascade_stats)
         # Evaluate behavior of individuals relative to threshold and stimuli
-        correct_state, behavior_stats = cs.evaluate_behavior(states = states, 
-                                                              thresholds = thresholds, 
-                                                              information = info_values, 
-                                                              types = types,
-                                                              behavior_df = behavior_stats)
+        correct_state, behavior_stats = evaluate_fitness_trial_behavior(states = states, 
+                                                                        thresholds = thresholds, 
+                                                                        information = info_values, 
+                                                                        types = types,
+                                                                        behavior_df = behavior_stats)
         
     # Return
     return behavior_stats, cascade_stats
+
+
+def evaluate_fitness_trial_behavior(states, thresholds, information, types, behavior_df):
+    # Evaluates the behavior of active individuals in the fitness trial cascade and updates data on correct/incorrect behavior.
+    #
+    # INPUTS:
+    # - states:           array listing the behavioral state of every individual (numpy array).
+    # - thresholds:       array of thresholds for each individual (numpy array).
+    # - information:      array of stimuli/infromation values (numpy array).
+    # - types:            array of type assignments for each individual (numpy array).
+    # - behavior_df:      dataframe to store the behavioral performance of individuals (pandas dataframe).
+    
+    # Assess what all individuals would have done if they had sampled info directly
+    correct_behavior = cs.evaluate_behavior(states = states, 
+                                             thresholds = thresholds, 
+                                             information = information, 
+                                             types = types)
+    correct_behavior = correct_behavior.reshape((-1, 1))
+    
+    # Assess error types, if desired by supplyin a behavior_df
+    true_positive = (states == 1) & correct_behavior #did behavior when they should have
+    true_negative = (states == 0) & ~correct_behavior  #did NOT do behavior when they should NOT have
+    false_positive = (states == 1) & ~correct_behavior  #did behavior when they should NOT have
+    false_negative = (states == 0) & correct_behavior  #did NOT do behavior when they should have
+    
+    # Update behavior tracking data
+    behavior_df['true_positive'] = behavior_df['true_positive'] + np.ndarray.flatten(true_positive)
+    behavior_df['true_negative'] = behavior_df['true_negative'] + np.ndarray.flatten(true_negative)
+    behavior_df['false_positive'] = behavior_df['false_positive'] + np.ndarray.flatten(false_positive)
+    behavior_df['false_negative'] = behavior_df['false_negative'] + np.ndarray.flatten(false_negative)
+    correct_behavior = np.ndarray.flatten(correct_behavior)
+    return correct_behavior, behavior_df
