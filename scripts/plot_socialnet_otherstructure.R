807f8e5cf73a1d4f6e5be18ac3e6ce67b0f0a7da
Chris Tokita <christopher.tokita@gmail.com>
2020-05-12 09:31:12 -0400

    carry out bayesian regression on social network data

--- scripts/plot_socialnet_otherstructure.R
+++ scripts/plot_socialnet_otherstructure.R
@@ -91,32 +91,123 @@ bayes_regression <- function(gammas, data) {
 # Filter to data of interest, fit regression
 ####################
 centrality_data <- network_change_data %>% 
-  filter(metric == "centrality")
-gammas <- sort( unique(centrality_data$gamma) )
-gammas <- gammas[c(3, 21)]
-reg_centrality <- bayes_regression(gammas = gammas, data = centrality_data)
+  filter(metric == "centrality") %>% 
+  split(.$gamma)
 
+# Check if regression fit already exists, otherwise conduct bayesian regression
+centrality_fit_file <- paste0(out_path, "regression_fits/centrality-threshold_cubic_fit.rds")
+if (file.exists(centrality_fit_file)) { 
+  regression_cent <- readRDS(centrality_fit_file)
+} else {
+  gamma_values <- as.numeric(names(centrality_data))
+  regression_cent <- brm_multiple(data = centrality_data,
+                                  formula = value ~ 1 + threshold + I(threshold^2) + I(threshold^3),
+                                  prior = c(prior(uniform(-10, 10), class = Intercept),
+                                            prior(normal(0, 10), class = b),
+                                            prior(normal(0, 50), class = sigma)),
+                                  iter = 3000,
+                                  warmup = 1000,
+                                  chains = 4,
+                                  seed = 323,
+                                  combine = FALSE)
+  saveRDS(regression_cent, file = centrality_fit_file)
+}
+
+# Get fitted values from model to data range/space
+x_values <- data.frame(threshold = seq(0, 1, 0.01))
+fit_cent <- lapply(seq(1:length(gamma_values)), function(i) {
+  gamma <- gamma_values[i]
+  fit_line <- fitted(regression_cent[[i]], newdata = x_values) %>% 
+    as.data.frame() %>% 
+    mutate(gamma = gamma,
+           threshold = seq(0, 1, 0.01))
+})
+fit_cent <- do.call("rbind", fit_cent)
 
 ####################
-# Plot eigenvector centrality vs thresold value for each info ecosystem (gamma)
+# Plot thresold vs eigenvector centrality value for each info ecosystem (gamma)
 ####################
-gg_centrality <- ggplot(reg_centrality, aes(x = threshold, y = Estimate, color = gamma, group = gamma)) +
-  # geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "grey80", color = NA, alpha = 0.2) +
-  geom_line() +
+gg_centrality <- ggplot(fit_cent, aes(x = threshold, y = Estimate, color = gamma, group = gamma)) +
+  # geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = gamma), color = NA, alpha = 0.3) +
+  geom_line(alpha = 0.8) +
   scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  scale_fill_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Centrality") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
   theme_ctokita() 
 gg_centrality
 
 
 
+gg_cent <- ggplot(network_change_data %>% filter(metric == "centrality", gamma == 1), aes(x = threshold, y = value, color = gamma, group = gamma)) +
+  # geom_bin2d() +
+  geom_point(size = 0.1, alpha = 0.2) +
+  stat_smooth(geom = 'line', size = 0.3, alpha = 0.8, se = FALSE) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  theme_ctokita()
+gg_cent
+
 ############################## Degree ##############################
+
+####################
+# Filter to data of interest, fit regression
+####################
+degree_data <- network_change_data %>% 
+  filter(metric == "degree") %>% 
+  split(.$gamma)
+
+# Check if regression fit already exists, otherwise conduct bayesian regression
+degree_fit_file <- paste0(out_path, "regression_fits/degree-threshold_cubic_fit.rds")
+if (file.exists(centrality_fit_file)) { 
+  regression_deg <- readRDS(degree_fit_file)
+} else {
+  gamma_values <- as.numeric(names(centrality_data))
+  regression_deg <- brm_multiple(data = degree_data,
+                                  formula = value ~ 1 + threshold + I(threshold^2) + I(threshold^3),
+                                  prior = c(prior(uniform(-10, 10), class = Intercept),
+                                            prior(normal(0, 10), class = b),
+                                            prior(normal(0, 50), class = sigma)),
+                                  iter = 3000,
+                                  warmup = 1000,
+                                  chains = 4,
+                                  seed = 323,
+                                  combine = FALSE)
+  saveRDS(regression_deg, file = centrality_data)
+}
+
+# Get fitted values from model to data range/space
+x_values <- data.frame(threshold = seq(0, 1, 0.01))
+fit_deg <- lapply(seq(1:length(gamma_values)), function(i) {
+  gamma <- gamma_values[i]
+  fit_line <- fitted(regression_deg[[i]], newdata = x_values) %>% 
+    as.data.frame() %>% 
+    mutate(gamma = gamma,
+           threshold = seq(0, 1, 0.01))
+})
+fit_deg <- do.call("rbind", fit_deg)
+
+
+####################
+# Plot thresold value vs degree for each info ecosystem (gamma)
+####################
+gg_degree <- ggplot(fit_deg, aes(x = threshold, y = Estimate, color = gamma, group = gamma)) +
+  # geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, fill = gamma), color = NA, alpha = 0.3) +
+  geom_line(alpha = 0.8) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  scale_fill_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Degree") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() 
+gg_degree
+
 
 # Degree 
 degree_data <- network_change_data %>% 
-  filter(metric == "degree")
+  filter(metric == "degree") %>% 
+  filter(gamma == 0.9)
 gg_degree <- ggplot(degree_data, aes(x = threshold, y = value, color = gamma, group = gamma)) +
   # geom_bin2d() +
-  # geom_point(size = 0.1, alpha = 0.2) +
+  geom_point(size = 0.1, alpha = 0.2) +
   stat_smooth(geom = 'line', size = 0.3, alpha = 0.8, se = FALSE) +
   scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
   theme_ctokita() 
