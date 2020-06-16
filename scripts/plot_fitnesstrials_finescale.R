cc3ef185d2e7b0c0b455c49a54d5b14dfab8e3a3
Chris Tokita <christopher.tokita@gmail.com>
2020-06-16 14:53:04 -0400

    plot individual scale fitness metrics

--- scripts/plot_fitnesstrials_finescale.R
+++ scripts/plot_fitnesstrials_finescale.R
@@ -38,7 +38,8 @@ pal <- brewer.pal(4, "PuOr")
 cascade_data <- read.csv(fit_cascade_path, header = TRUE) %>% 
   gather(metric, value, -trial, -gamma) %>% 
   mutate(trial = factor(trial, levels = c("pre", "post"))) %>% 
-  mutate(gamma = round(gamma, digits = 2)) #weird representation of the numbers that aren't precise
+  mutate(trial = recode(trial,"pre" = "Pre-NB", "post" = "post-NB"),
+         gamma = round(gamma, digits = 2)) #weird representation of the numbers that aren't precise
 
 
 ####################
@@ -60,36 +61,209 @@ gg_sizedist <- ggplot(avgsize, aes(fill = trial)) +
   theme_ctokita() +
   facet_grid(trial~gamma)
 gg_sizedist #show plot before saving
-ggsave(plot = gg_size, filename = paste0(out_path, "cascadesize", plot_tag ,".png"), width = 75, height = 45, units = "mm", dpi = 400)
 
 ####################
 # Plot: Cascade bias
 ####################
 # Filter
-bias <- cascade_sum %>% 
-  filter(metric == "cascade_bias")
+bias <- cascade_data %>% 
+  filter(metric == "cascade_bias",
+         gamma %in% seq(-1, 1, 0.5))
 
 # Plot
-gg_bias <- ggplot(bias, aes(x = trial, y = mean, color = gamma, group = gamma)) +
-  geom_line(size = 0.3, alpha = 0.8) +
-  geom_point(size = 0.8) +
-  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
-  scale_x_discrete(labels = c("Pre", "Post")) +
-  scale_y_continuous(limits = c(0.1, 0.7), breaks = seq(0.1, 0.7, 0.1)) +
+gg_bias <- ggplot(bias, aes(fill = trial)) +
+  geom_histogram(aes(x = value)) +
   ylab("Cascade bias") +
   xlab("Fitness trial") +
-  theme_ctokita()
+  theme_ctokita() +
+  facet_grid(trial~gamma)
 gg_bias #show plot before saving
-ggsave(plot = gg_bias, filename = paste0(out_path, "cascadebias", plot_tag ,".png"), width = 75, height = 45, units = "mm", dpi = 400)
 
 
-############################## Ftiness trials: Behavior ##############################
+
+############################## Ftiness trials: Behavior, compare to thresholds ##############################
 
 ####################
 # Load data 
 ####################
 # Read in data
 behav_data <- read.csv(fit_behavior_path, header = TRUE) %>% 
-  gather(metric, value, -gamma, -trial) %>% 
-  mutate(trial = factor(trial, levels = c("pre", "post")))
+  mutate(active = true_positive + false_positive) %>% 
+  gather(metric, value, -gamma, -trial, -threshold) %>% 
+  mutate(trial = factor(trial, levels = c("pre", "post"))) %>% 
+  mutate(trial = recode(trial,"pre" = "Initial network", "post" = "Final network"))
+
+# Select gammas
+gamma_vals <- c(-1, -0.4, 0.0, 0.6, 0.8, 1)
+
+
+####################
+# Plot: Total activity rates vs threshold
+####################
+# Filter
+activity <- behav_data %>% 
+  filter(metric == "active",
+         gamma %in% gamma_vals)
+
+# Plot
+gg_act <- ggplot(activity, aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Activity") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_act
+ggsave(gg_act, filename = paste0(out_path, "threshold-activity.png"), width = 90, height = 60, units = "mm")
+
+####################
+# Plot: Base behavior rates vs threshold
+####################
+# Filter
+baserates <- behav_data %>% 
+  filter(metric %in% c("false_positive", "false_negative", "true_positive", "true_negative"),
+         gamma %in% gamma_vals)
+
+# True Positive
+gg_tp <- ggplot(baserates %>% filter(metric == "true_positive"), aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("True positive") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_tp #show plot before saving
+ggsave(gg_tp, filename = paste0(out_path, "threshold-truepositive.png"), width = 90, height = 60, units = "mm")
+
+# False Positive
+gg_fp <- ggplot(baserates %>% filter(metric == "false_positive"), aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("False positive") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_fp #show plot before saving
+ggsave(gg_fp, filename = paste0(out_path, "threshold-falsepositive.png"), width = 90, height = 60, units = "mm")
+
+# True Negative
+gg_tn <- ggplot(baserates %>% filter(metric == "true_negative"), aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("True negative") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_tn #show plot before saving
+ggsave(gg_tn, filename = paste0(out_path, "threshold-truenegative.png"), width = 90, height = 60, units = "mm")
+
+# False Negative
+gg_fn <- ggplot(baserates %>% filter(metric == "false_negative"), aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("False negative") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_fn #show plot before saving
+ggsave(gg_fn, filename = paste0(out_path, "threshold-falsenegative.png"), width = 90, height = 60, units = "mm")
+
+
+####################
+# Plot: Sensitivity vs threshold
+####################
+# Filter
+sens <- behav_data %>% 
+  filter(metric == "sensitivity",
+         gamma %in% gamma_vals)
+
+# Plot
+gg_sens <- ggplot(sens, aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Sensitivity") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_sens #show plot before saving
+ggsave(gg_sens, filename = paste0(out_path, "threshold-sensitivity.png"), width = 90, height = 60, units = "mm")
+
+
+####################
+# Plot: Specificity vs threshold
+####################
+# Filter
+spec <- behav_data %>% 
+  filter(metric == "specificity",
+         gamma %in% gamma_vals)
+
+# Plot
+gg_spec <- ggplot(spec, aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Specificity") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_spec #show plot before saving
+ggsave(gg_spec, filename = paste0(out_path, "threshold-specificity.png"), width = 90, height = 60, units = "mm")
+
+
+####################
+# Plot: Precision vs threshold
+####################
+# Filter
+prec <- behav_data %>% 
+  filter(metric == "precision",
+         gamma %in% gamma_vals)
+
+# Plot
+gg_prec <- ggplot(prec, aes(x = threshold, y = value, color = gamma)) +
+  geom_point(size = 0.2, alpha = 0.2, stroke = 0) +
+  scale_x_continuous(breaks = seq(0, 1, 1)) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  ylab("Precision") +
+  xlab(expression(paste("Threshold, ", theta[i]))) +
+  theme_ctokita() +
+  theme(legend.position = "none",
+        aspect.ratio = 2,
+        strip.text.y = element_text(face = "bold")) +
+  facet_grid(trial~gamma, 
+             labeller = label_bquote(cols = gamma == .(gamma)))
+gg_prec #show plot before saving
+ggsave(gg_prec, filename = paste0(out_path, "threshold-precision.png"), width = 90, height = 60, units = "mm")
 
