abf0cbecd9c0d75937351bbbd3913bd888c544e5
Chris Tokita <christopher.tokita@gmail.com>
2020-06-22 14:58:11 -0400

    behavior rate plots by threshold

--- scripts/plot_fitnesstrials.R
+++ scripts/plot_fitnesstrials.R
@@ -29,6 +29,7 @@ plot_color <- "#1B3B6F"
 pal <- brewer.pal(4, "PuOr")
 
 
+
 ############################## Ftiness trials: Cascade dynamics ##############################
 
 ####################
@@ -111,6 +112,7 @@ ggsave(plot = gg_bias, filename = paste0(out_path, "cascadebias", plot_tag ,".pn
 ggsave(plot = gg_bias + theme(legend.position = "none"), filename = paste0(out_path, "cascadebias", plot_tag ,".svg"), width = 45, height = 45, units = "mm", dpi = 400)
 
 
+
 ############################## Ftiness trials: Behavior ##############################
 
 ####################
@@ -147,13 +149,13 @@ gg_behavrates <- ggplot(behavrates, aes(x = trial, y = mean, color = gamma, grou
   scale_x_discrete(labels = c("Initial network", "Final network")) +
   ylab("Behavior frequency") +
   theme_ctokita() +
-  theme(strip.background = element_rect(color = NA, fill = "grey90"),
-        axis.line = element_line(),
+  theme(axis.line = element_line(),
         axis.title.x = element_blank()) +
-  facet_wrap(~metric, scales = "free_x")
+  facet_wrap(~metric, scales = "free")
 gg_behavrates #show plot before saving
 ggsave(plot = gg_behavrates, filename = paste0(out_path, "behaviorrates", plot_tag ,".png"), width = 120, height = 90, units = "mm", dpi = 400)
-ggsave(plot = gg_behavrates + facet_wrap(~metric, scales = "free"), filename = paste0(out_path, "behaviorrates_rescaled", plot_tag ,".png"), width = 120, height = 90, units = "mm", dpi = 400)
+ggsave(plot = gg_behavrates, filename = paste0(out_path, "behaviorrates", plot_tag ,".svg"), width = 120, height = 90, units = "mm", dpi = 400)
+ggsave(plot = gg_behavrates + facet_wrap(~metric, scales = "fixed"), filename = paste0(out_path, "behaviorrates_fixedscale", plot_tag ,".png"), width = 120, height = 90, units = "mm", dpi = 400)
 
 
 
@@ -216,3 +218,89 @@ gg_prec <- ggplot(precision, aes(x = trial, y = mean, color = gamma, group = gam
   theme(axis.title.x = element_blank())
 gg_prec #show plot before saving
 ggsave(plot = gg_prec, filename = paste0(out_path, "precision", plot_tag ,".png"), width = 75, height = 45, units = "mm", dpi = 400)
+
+
+
+############################## Ftiness trials: Behavior, by high and low threshold ##############################
+
+####################
+# Load data 
+####################
+# Read in data
+behav_data <- read.csv(fit_behavior_path, header = TRUE)
+
+# Summarise
+behav_sum_lowthresh <- behav_data %>% 
+  filter(threshold < 0.25) %>% 
+  gather(metric, value, -gamma, -trial) %>% 
+  mutate(trial = factor(trial, levels = c("pre", "post"))) %>% 
+  group_by(trial, gamma, metric) %>% 
+  summarise(mean = mean(value, na.rm = TRUE),
+            sd = sd(value, na.rm = TRUE),
+            ci95 = qnorm(0.975) * sd(value, na.rm = TRUE) / sqrt( sum(!is.na(value)) )) #denominator removes NA values from count
+
+behav_sum_highthresh <- behav_data %>% 
+  filter(threshold > 0.75) %>% 
+  gather(metric, value, -gamma, -trial) %>% 
+  mutate(trial = factor(trial, levels = c("pre", "post"))) %>% 
+  group_by(trial, gamma, metric) %>% 
+  summarise(mean = mean(value, na.rm = TRUE),
+            sd = sd(value, na.rm = TRUE),
+            ci95 = qnorm(0.975) * sd(value, na.rm = TRUE) / sqrt( sum(!is.na(value)) )) #denominator removes NA values from count
+
+####################
+# Plot: Base behavior rates, low threshold
+####################
+# Filter
+behavrates_lowthresh <- behav_sum_lowthresh %>% 
+  filter(metric %in% c("true_positive", "true_negative", "false_positive", "false_negative")) %>% 
+  mutate(metric = factor(metric, levels = c("true_positive", "false_positive", "false_negative", "true_negative"))) %>% 
+  mutate(news = NA, 
+         reaction = NA)
+levels(behavrates_lowthresh$metric) <- c("TP", "FP", "FN", "TN")
+
+# Plot
+gg_behavrates_lowthresh <- ggplot(behavrates_lowthresh, aes(x = trial, y = mean, color = gamma, group = gamma)) +
+  geom_line(size = 0.3, alpha = 0.8) +
+  geom_point(size = 0.8) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  scale_x_discrete(labels = c("Initial", "Final")) +
+  ylab("Behavior frequency") +
+  theme_ctokita() +
+  theme(strip.text = element_blank(),
+        legend.position = "none",
+        axis.line = element_line(),
+        axis.title.x = element_blank()) +
+  facet_wrap(~metric, scales = "free")
+gg_behavrates_lowthresh #show plot before saving
+ggsave(plot = gg_behavrates_lowthresh, filename = paste0(out_path, "behaviorrates_lowthresh", plot_tag ,".png"), width = 55, height = 45, units = "mm", dpi = 400)
+ggsave(plot = gg_behavrates_lowthresh, filename = paste0(out_path, "behaviorrates_lowthresh", plot_tag ,".svg"), width = 55, height = 45, units = "mm")
+
+
+####################
+# Plot: Base behavior rates, high threshold
+####################
+# Filter
+behavrates_highthresh <- behav_sum_highthresh %>% 
+  filter(metric %in% c("true_positive", "true_negative", "false_positive", "false_negative")) %>% 
+  mutate(metric = factor(metric, levels = c("true_positive", "false_positive", "false_negative", "true_negative"))) %>% 
+  mutate(news = NA, 
+         reaction = NA)
+levels(behavrates_highthresh$metric) <- c("TP", "FP", "FN", "TN")
+
+# Plot
+gg_behavrates_highthresh <- ggplot(behavrates_highthresh, aes(x = trial, y = mean, color = gamma, group = gamma)) +
+  geom_line(size = 0.3, alpha = 0.8) +
+  geom_point(size = 0.8) +
+  scale_color_gradientn(colors = pal, name = expression(paste("Information\necosystem", gamma))) +
+  scale_x_discrete(labels = c("Initial", "Final")) +
+  ylab("Behavior frequency") +
+  theme_ctokita() +
+  theme(strip.text = element_blank(),
+        axis.line = element_line(),
+        axis.title.x = element_blank(),
+        legend.position = "none") +
+  facet_wrap(~metric, scales = "free")
+gg_behavrates_highthresh #show plot before saving
+ggsave(plot = gg_behavrates_highthresh, filename = paste0(out_path, "behaviorrates_highthresh", plot_tag ,".png"), width = 55, height = 45, units = "mm", dpi = 400)
+ggsave(plot = gg_behavrates_highthresh, filename = paste0(out_path, "behaviorrates_highthresh", plot_tag ,".svg"), width = 55, height = 45, units = "mm")
