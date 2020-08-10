037d860a6fad7157eb64b393e4a4ad5628cfc9f2
Chris Tokita <christopher.tokita@gmail.com>
2020-08-10 12:30:43 -0400

    Create prepare_ec2_instance.sh

---
+++ empirical/scripts/prepare_ec2_instance.sh
@@ -0,0 +1,10 @@
+# Get Anaconda3
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh
+source ~/.bashrc
+
+# Install pip
+conda install pip
+
+# Install needed packages for our scripts
+pip install -r requirements 
