dc6f0a6cbebe63847f4bfdd85aad798afcb28b0f
Chris Tokita <christopher.tokita@gmail.com>
2020-08-10 16:18:07 -0400

    Update prepare_ec2_instance.sh

--- empirical/scripts/prepare_ec2_instance.sh
+++ empirical/scripts/prepare_ec2_instance.sh
@@ -1,3 +1,6 @@
+# Example terminal command to get into ec2 instance:
+# > ssh -i ../data/api_keys/aws_pem/my_key_here.pem ec2-user@[insert Public DNS (IPv4)]
+
 # Get Anaconda3
 wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 bash Anaconda3-2020.02-Linux-x86_64.sh
