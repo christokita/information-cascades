4988b789358406ef5b57e97a10fa976216d5a701
Chris Tokita <christopher.tokita@gmail.com>
2020-12-15 16:55:01 -0500

    update script to process rd 1 survey data

--- experimental/scipts/01_process_survey_rd1.py
+++ experimental/scipts/01_process_survey_rd1.py
@@ -6,7 +6,7 @@ Created on Wed Sep 23 12:04:45 2020
 @author: ChrisTokita
 
 SCRIPT
-Check to confirm participants followed the requested news source.
+Process our participant recruitment from the first round of the survey.
 """
 
 ####################
@@ -37,13 +37,13 @@ mturk_survey_file = 'mturk_survey_rd1_raw.xlsx'
 fb_survey_file = 'fb_survey_rd1_raw.xlsx'
 
 # Twitter token file
-token_file = '../api_keys/twitter_tokens/ckt_tokens1.json'
+token_files = ['../api_keys/twitter_tokens/ckt_tokens1.json', '../api_keys/twitter_tokens/ag_tokens1.json']
 
 # Get dropbox token
 with open('../api_keys/dropbox_token/dropbox_token.json') as f:
     key = json.load(f)
 dropbox_token = key.get('access_token')
-del key
+del key, f
 
 
 ####################
@@ -70,12 +70,13 @@ raw_survey_data_mturk = raw_survey_data_mturk.iloc[1:] #first row is question te
 raw_survey_data_mturk = raw_survey_data_mturk.reset_index(drop = True)
 raw_survey_data_mturk['email'] = '' #we didn't collect email in Mturk, but did in FB survey
 raw_survey_data_mturk['email_confirm'] = ''
-raw_survey_data_mturk['survey_deployment'] = 'mturk'
+raw_survey_data_mturk['recruited_from'] = 'mturk'
 
 raw_survey_data_fb = sheet_fb.range('A1').options(pd.DataFrame, header = True, index = False, expand='table').value
 raw_survey_data_fb = raw_survey_data_fb.iloc[1:] #first row is question text
 raw_survey_data_fb = raw_survey_data_fb.reset_index(drop = True)
-raw_survey_data_fb['survey_deployment'] = 'fb'
+raw_survey_data_fb['recruited_from'] = 'fb'
+del sheet_mturk, sheet_fb, wb_mturk, wb_facebook
 
 # Delete downloaded file
 shutil.rmtree(tmp_dir)
@@ -92,6 +93,7 @@ raw_survey_data_fb = raw_survey_data_fb[mutual_columns]
 # Combine
 raw_survey_data = raw_survey_data_mturk.append(raw_survey_data_fb, ignore_index = True)
 raw_survey_data['username_manual_compile'] = raw_survey_data['username_manual_compile'].str.strip(to_strip = ' ') #remove any lurking leading/trailing spaces
+del raw_survey_data_mturk, raw_survey_data_fb
 
 # Only keep particpants who finished the survey  (doesn't really seem to affect anything since Qualtrics only gives completed answers)
 do_not_pay = pd.DataFrame(data = raw_survey_data.qid[raw_survey_data.Finished == 'False'], columns = ['qid'])
@@ -117,19 +119,19 @@ treatment_counts = shared_username.groupby(['ideology', 'hi_corr']).size() #mode
 # We manually will check which one is the legitimate one (usually, which based on which news account they are actually following)
 duplicated_usernames = shared_username['username_manual_compile'][shared_username.duplicated(subset = ['username_manual_compile'])]
 check_duplicates = shared_username[shared_username['username_manual_compile'].isin(duplicated_usernames)]
-check_duplicates = check_duplicates[['username_manual_compile', 'news_source', 'qid', 'survey_deployment', 'username_source']]
-duplicates_to_drop = ['305894', '586007', '345488'] #these were manually checked. See lab notebook.
+check_duplicates = check_duplicates[['username_manual_compile', 'news_source', 'qid', 'recruited_from', 'username_source']]
+duplicates_to_drop = ['305894', '586007', '345488', '368149', '899029'] #these were manually checked. See lab notebook.
 shared_username = shared_username[~shared_username['qid'].isin(duplicates_to_drop)]
 
 # Create list of partipants that will form basis of our crosswalk
-user_crosswalk = shared_username[['qid', 'username_manual_compile', 'email', 'survey_deployment']].copy()
+user_crosswalk = shared_username[['qid', 'username_manual_compile', 'email']].copy()
 user_crosswalk['username_manual_compile'] = user_crosswalk['username_manual_compile'].str.replace("^.*@", "") #clean up user names by removing leading "@". Some people put their name before the @.
 user_crosswalk['username_manual_compile'] = user_crosswalk['username_manual_compile'].str.replace(" [^a-zA-Z0-9]+", "") #one person put a trailing marks at the end of the entry
 user_crosswalk['username_manual_compile'] = user_crosswalk['username_manual_compile'].str.strip() #remove trailing/leading space
 user_crosswalk = user_crosswalk.rename(columns = {'username_manual_compile': 'survey_user_name'})
 
 # Remove twitter identifying data & irrelevant columns from survey
-# This will be our set of survey data we can save for use later.
+# This will be our set of survey data we can save for use later (after we filter to just the users of interest)
 survey_data = shared_username.drop(columns = ['twitterhandle', 'username_manual_compile', 'username_backup',
                                               'email', 'email_confirm',
                                               'Status', 'IPAddress', 'Progress',
@@ -164,7 +166,11 @@ logger.addHandler(file_handler)
 logger.info("Loading Twitter tokens...")
 
 # Load all tokens
-all_tokens = twee.load_tokens(path = token_file, logger = logger)
+all_tokens = {}
+for i in np.arange(len(token_files)):
+    token = twee.load_tokens(path = token_files[i], logger = logger)
+    all_tokens.update( { str(i+1): token['1'] } )
+
 
 # Set the initial token key to select the first token
 token_key = "1"
@@ -221,13 +227,37 @@ user_crosswalk = user_crosswalk.drop(columns = ['survey_user_name'])
 
 
 ####################
+# Create our final set of particpants
+####################
+# Create dataset of relevant user info
+participants = user_crosswalk.merge(survey_data[['qid', 'hi_corr', 'ideology']], on = 'qid', how = 'left')
+participants = participants.merge(twitter_info[['user_id', 'user_id_str', 'user_name', 'protected']], on = ['user_id', 'user_id_str'], how = 'left')
+
+# Flag who is in the final user pool based on our criteria of:
+#     (1) provided real twitter username
+#     (2) provided a twitter account that is NOT private
+#     (3) provided us way to contact them, i.e., email for those we recruited from FB
+valid_username = ~pd.isna(participants['user_id_str']) 
+protected_account = participants['protected'].astype(bool)
+fb_no_email = (participants.recruited_from == 'fb') & pd.isna(participants['email'])
+participants['final_user_pool'] = valid_username & ~fb_no_email & ~protected_account 
+
+# Narrow down our survey data, user_crosswalk, and participants to only those who are in the final pool
+final_participants = participants[participants.final_user_pool == True].copy()
+final_twitter_info = twitter_info[twitter_info['user_id_str'].isin(final_participants.user_id_str)]
+final_survey_data = survey_data[survey_data['qid'].isin(final_participants['qid'])].copy()
+final_user_crosswalk = user_crosswalk[user_crosswalk['qid'].isin(final_participants['qid'])].copy()
+
+final_survey_data[['ideology', 'hi_corr']].value_counts()
+
+
+####################
 # Confirm they are following news source
 ####################
 # Create temporary dataset of users and the news source they are following
-users_to_check = survey_data[['qid', 'news_source']].copy()
-users_to_check = users_to_check.merge(user_crosswalk, on = 'qid')
-users_to_check = users_to_check[~pd.isna(users_to_check['user_id'])].reset_index(drop = True)
-users_to_check['following_news_source'] = np.nan
+users_to_check = final_survey_data[['qid', 'news_source']].copy()
+users_to_check = users_to_check.merge(final_user_crosswalk, on = 'qid')
+users_to_check['following_newssource_postrd1'] = np.nan
 
 # loop through users to confirm they are following
 for j in range(users_to_check.shape[0]):
@@ -244,39 +274,13 @@ for j in range(users_to_check.shape[0]):
     follow_obj = follow_check[0]
     
     # Append data
-    users_to_check.loc[j, 'following_news_source'] =  follow_obj.following
+    users_to_check.loc[j, 'following_newssource_postrd1'] =  follow_obj.following
     del follow_check, follow_obj
     
 # Note who is following news sources among our particpants
-twitter_info = twitter_info.merge(users_to_check[['user_id', 'user_id_str', 'following_news_source']], 
-                                  on = ['user_id', 'user_id_str'],
-                                  how = 'left')
-
-
-####################
-# Create our final set of particpants
-####################
-# Create dataset of relevant user info
-participants = user_crosswalk.merge(survey_data[['qid', 'hi_corr', 'ideology']], on = 'qid', how = 'left')
-participants = participants.merge(twitter_info[['user_id', 'user_id_str', 'user_name', 'protected', 'following_news_source']], on = ['user_id', 'user_id_str'], how = 'left')
-
-# Flag who is in the final user pool based on our criteria of:
-#     (1) provided real twitter username
-#     (2) provided a twitter account that is NOT private
-#     (3) provided us way to contact them, i.e., email for those we recruited from FB
-valid_username = ~pd.isna(participants['user_id_str']) 
-protected_account = participants['protected'].astype(bool)
-fb_no_email = (participants.survey_deployment == 'fb') & pd.isna(participants['email'])
-participants['final_user_pool'] = valid_username & ~fb_no_email & ~protected_account 
-
-# Narrow down our survey data, user_crosswalk, and participants to only those who are in the final pool
-final_participants = participants[participants.final_user_pool == True].copy()
-final_twitter_info = twitter_info[twitter_info['user_id_str'].isin(final_participants.user_id_str)]
-final_survey_data = survey_data[survey_data['qid'].isin(final_participants['qid'])].copy()
-final_user_crosswalk = user_crosswalk[user_crosswalk['qid'].isin(final_participants['qid'])].copy()
-final_user_crosswalk['final_user_pool'] = True
-
-final_survey_data[['ideology', 'hi_corr']].value_counts()
+final_twitter_info = final_twitter_info.merge(users_to_check[['user_id', 'user_id_str', 'following_newssource_postrd1']], 
+                                              on = ['user_id', 'user_id_str'],
+                                              how = 'left')
 
 
 ####################
@@ -287,15 +291,17 @@ high_corr_group = final_participants[final_participants.hi_corr == '1'].copy()
 low_corr_group = final_participants[final_participants.hi_corr == '0'].copy()
 
 # Assign survey wave
-def assign_survey_wave(df):
+def assign_survey_wave(df, seed):
+    np.random.seed(seed)
     n_rows = df.shape[0]
     n_repeats = math.ceil(n_rows / 3)
     batch_assignments = np.tile([1, 2, 3], n_repeats)
     batch_assignments = batch_assignments[0:n_rows]
+    batch_assignments = np.random.choice(batch_assignments, size = n_rows, replace = False) #shuffle around
     return batch_assignments
     
-high_corr_group['survey_wave'] = assign_survey_wave(high_corr_group)
-low_corr_group['survey_wave'] = assign_survey_wave(low_corr_group)
+high_corr_group['survey_wave'] = assign_survey_wave(high_corr_group, seed = 609)
+low_corr_group['survey_wave'] = assign_survey_wave(low_corr_group, seed = 323)
 
 # Create set of arrays for use in follow up survey
 def create_survey_wave_lists(df):
@@ -321,6 +327,7 @@ final_user_crosswalk.to_excel(tmp_dir + 'user_crosswalk.xlsx', index = False)
 do_not_pay.to_csv(tmp_dir + 'donotpay_rd1.csv', index = False)
 final_survey_data.to_csv(tmp_dir + 'survey_data_rd1.csv', index = False)
 final_twitter_info.to_csv(tmp_dir + 'participant_twitter_info.csv', index = False)
+
 with open(tmp_dir + 'survey2_high_corr_waves.txt', 'w') as filehandle:
     filehandle.writelines("%s\n" % x for x in high_corr_waves)
 with open(tmp_dir + 'survey2_low_corr_waves.txt', 'w') as filehandle:
@@ -350,7 +357,7 @@ tmp_dir = "/Users/ChrisTokita/Documents/Research/Tarnita Lab/Information Cascade
 os.mkdir(tmp_dir)
 
 # Determine who won payment
-fb_participants = raw_survey_data[raw_survey_data.survey_deployment == 'fb']
+fb_participants = raw_survey_data[raw_survey_data.recruited_from == 'fb']
 n_giftcards = math.ceil(fb_participants.shape[0] / 25) #round up to nearest 25 to make sure people are paid more than not
 fb_participants_with_emails = fb_participants[~pd.isna(fb_participants.email)]
 pay_these_participants = fb_participants_with_emails['email'].sample(n = n_giftcards, replace = False, random_state = 609)
