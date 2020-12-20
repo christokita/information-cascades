3ea80121c1b113b173634f1db527bf5e1b42b2ec
Chris Tokita <christopher.tokita@gmail.com>
2020-12-20 18:10:57 -0500

    Update 02_monitor_experimentalnetworks.py

--- experimental/scipts/02_monitor_experimentalnetworks.py
+++ experimental/scipts/02_monitor_experimentalnetworks.py
@@ -17,7 +17,6 @@ import tweepy
 import logging
 import pandas as pd
 import numpy as np
-import math
 import xlwings
 import dropbox
 import json
@@ -121,8 +120,18 @@ API = twee.set_api_keys(consumer_key = consumer_key,
 ####################
 # Pull friend and follower lists for each of our users of interest
 ####################
+# Remove users we already collected (in case we need to run this script again due to error)
+users_to_check = user_crosswalk['user_id']
+friend_list_files = os.listdir(outpath + "friend_lists/")
+follower_list_files = os.listdir(outpath + "follower_lists/")
+have_friend_list = [x for x in users_to_check if 'friends_{}_{}.csv'.format(x, retrieval_date) in friend_list_files]
+have_follower_list = [x for x in users_to_check if 'followers_{}_{}.csv'.format(x, retrieval_date) in follower_list_files]
+have_all_data = np.intersect1d(have_friend_list, have_follower_list) #only remove users that we have both a friend and follower list for
+users_to_check = users_to_check[~users_to_check.isin(have_all_data)]
+
 # Loop through users and get their friend/follower ID lists
-for user_id in user_crosswalk['user_id']:
+error_users = []
+for user_id in users_to_check:
     
     # List to collect friends and followers
     friend_ids = []
@@ -134,10 +143,20 @@ for user_id in user_crosswalk['user_id']:
         logger.info("Trying to switch tokens to ensure we don't hit a rate limit.")
         print("Trying to switch tokens to ensure we don't hit a rate limit.")
         API = twee.switch_token(API, all_tokens, logger)
-        
-    for page in tweepy.Cursor(API.friends_ids, user_id = user_id).pages():
-        friend_ids.extend(page)
-        del page
+     
+    try:    
+        # Page through friends
+        for page in tweepy.Cursor(API.friends_ids, user_id = user_id).pages():
+            friend_ids.extend(page)
+            del page
+            
+        # Write to file
+        friends_df = pd.DataFrame({'user_id': friend_ids, 'user_id_str': ["\"" + str(x) + "\"" for x in friend_ids]})
+        friend_file_name = "friends_{}_{}.csv".format(user_id, retrieval_date)
+        friends_df.to_csv(outpath + "friend_lists/" + friend_file_name, index = False)
+    except:
+        error_users.append(user_id)
+        print("Error with getting friends of user %s. Skipping..." % user_id)
     
     # Check if we need to switch tokens, then get followers
     switch_token = twee.rate_limit_check_specific(API, all_tokens, logger, query_of_interest = "followers")
@@ -146,17 +165,18 @@ for user_id in user_crosswalk['user_id']:
         print("Trying to switch tokens to ensure we don't hit a rate limit.")
         API = twee.switch_token(API, all_tokens, logger)
     
-    for page in tweepy.Cursor(API.followers_ids, user_id = user_id).pages():
-        follower_ids.extend(page)
-        del page
-        
-    # Write to file
-    friends_df = pd.DataFrame({'user_id': friend_ids, 'user_id_str': ["\"" + str(x) + "\"" for x in friend_ids]})
-    friend_file_name = "friends_{}_{}.csv".format(user_id, retrieval_date)
-    friends_df.to_csv(outpath + "friend_lists/" + friend_file_name, index = False)
+    try:    
+        # Page through followers
+        for page in tweepy.Cursor(API.followers_ids, user_id = user_id).pages():
+            follower_ids.extend(page)
+            del page
     
-    followers_df = pd.DataFrame({'user_id': follower_ids, 'user_id_str': ["\"" + str(x) + "\"" for x in follower_ids]})
-    follower_file_name = "followers_{}_{}.csv".format(user_id, retrieval_date)
-    followers_df.to_csv(outpath + "follower_lists/" + follower_file_name, index = False)
+        # Write to file
+        followers_df = pd.DataFrame({'user_id': follower_ids, 'user_id_str': ["\"" + str(x) + "\"" for x in follower_ids]})
+        follower_file_name = "followers_{}_{}.csv".format(user_id, retrieval_date)
+        followers_df.to_csv(outpath + "follower_lists/" + follower_file_name, index = False)
+    except:
+        error_users.append(user_id)
+        print("Error with getting followers of user %s. Skipping..." % user_id)
 
 
