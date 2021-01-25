d58a5871886679158a2327b535c3b4d0d2773a58
Chris Tokita <christopher.tokita@gmail.com>
2021-01-25 16:27:11 -0500

    Update 03_construct_experimentalnetworks.py

--- experimental/scipts/03_construct_experimentalnetworks.py
+++ experimental/scipts/03_construct_experimentalnetworks.py
@@ -34,7 +34,7 @@ path_to_twitter_data = dropbox_dir + 'twitter_data/'
 crosswalk_file = 'user_crosswalk.xlsx'
 survey_file = 'survey_data_rd1.csv'
 
-# Path to networks
+# Path to friend/follower lists
 network_data_dir = "/Volumes/CKT-DATA/information-cascades/experimental/data/"
 
 # Get dropbox token
@@ -80,6 +80,8 @@ high_corr_users = high_corr_users.sort_values(by = 'user_id')
 
 
 
+############################### Initial follow networks ###############################
+
 ####################
 # Construct initial network
 ####################
@@ -92,7 +94,7 @@ def construct_initial_network(user_ids, initial_friendfollower_files):
     for user in user_ids:
         
         # Grab friend/follower files
-        user_files = [x for x in friendfollower_files if re.search(user, x)]
+        user_files = [x for x in initial_friendfollower_files if re.search(user, x)]
         
         # Users who were protected didn't get friend/follower lists
         if len(user_files) == 0:
@@ -170,4 +172,99 @@ for file in ['lowcorr_initial_network.csv', 'highcorr_initial_network.csv']:
         dbx.files_upload(f.read(), path = path_to_twitter_data + file, mode = dropbox.files.WriteMode.overwrite)  
  
 # Delete files
-shutil.rmtree(tmp_dir)  
+shutil.rmtree(tmp_dir) 
+
+
+
+############################### Final follow networks ###############################
+
+####################
+# Construct final network
+####################
+# Function to construct initial networks
+def construct_final_network(user_ids, final_friendfollower_files):
+    
+    # Loop through users in this tereatment group
+    friend_network = pd.DataFrame(columns = user_ids) #who follows whom
+    follower_network = pd.DataFrame(columns = user_ids) #who is followed by whom
+    for user in user_ids:
+        
+        # Grab friend/follower files
+        user_files = [x for x in final_friendfollower_files if re.search(user, x)]
+        
+        # Users who were protected didn't get friend/follower lists
+        if len(user_files) == 0:
+            participants_followed = np.repeat(False, len(user_ids))
+            participants_following = np.repeat(False, len(user_ids))
+            
+        # Otherwise, grab appropriate friend/follower lists
+        else: 
+            user_follower_file = [x for x in user_files if re.search('followers', x)][0]
+            user_friend_file = [x for x in user_files if re.search('friends', x)][0]
+            
+            # Determine who they are following
+            friends = pd.read_csv(network_data_dir + 'friend_lists/' + user_friend_file, dtype = {'user_id': object, 'user_id_str': object})
+            friends['user_id'] = friends['user_id_str'].str.replace("\"", "")
+            followers = pd.read_csv(network_data_dir + 'follower_lists/' + user_follower_file, dtype = {'user_id': object, 'user_id_str': object})
+            followers['user_id'] = friends['user_id_str'].str.replace("\"", "")
+    
+            participants_followed = np.array( [x in friends['user_id'].values for x in user_ids] )
+            participants_following = np.array( [x in followers['user_id'].values for x in user_ids] )
+                    
+        # Append to dataframes
+        new_friend_row = pd.DataFrame([participants_followed.astype(int)], columns = user_ids, index = [user])
+        friend_network = friend_network.append(new_friend_row)
+        
+        new_follower_row = pd.DataFrame([participants_following.astype(int)], columns = user_ids, index = [user])
+        follower_network = follower_network.append(new_follower_row)
+        
+    # End function    
+    return friend_network, follower_network 
+
+
+# Get list of friend/follower files
+# NOTE: two users changed to Protected status during space between initial recruitment and monitoring of friend/follower network
+# As a result, we have 307 total initial friend/follower lists
+follower_list_files = os.listdir(network_data_dir + 'follower_lists')
+friend_list_files = os.listdir(network_data_dir + 'friend_lists')
+
+# Combine friend and follower lists into one to make parsing easier, narrow to just the date in which we pulled final follower lists
+friendfollower_files = follower_list_files + friend_list_files
+final_friendfollower_files = [x for x in friendfollower_files if re.search('2021-01-20', x)]
+
+# Construct networks
+lowcorr_final_network, lowcorr_final_follower_network = construct_final_network(user_ids = low_corr_users['user_id'], final_friendfollower_files = final_friendfollower_files)
+highcorr_final_network, highcorr_final_follower_network = construct_final_network(user_ids = high_corr_users['user_id'], final_friendfollower_files = final_friendfollower_files)
+
+# Determine which users we do not have lists for (for accounts that were Protected when we collected friend/follower lists)
+unique_users_with_data = [re.search("[a-z]+_([0-9]+)_.*", x).group(1) for x in final_friendfollower_files]
+unique_users_with_data = np.unique(unique_users_with_data)
+lowcorr_users_without_list = [x for x in low_corr_users['user_id'] if x not in unique_users_with_data] #only one user
+highcorr_users_without_list =  [x for x in high_corr_users['user_id'] if x not in unique_users_with_data] #only one user
+
+# Fill in missing data
+for user_without_list in lowcorr_users_without_list:
+    lowcorr_final_network.loc[user_without_list, :] = lowcorr_final_follower_network[user_without_list]
+    
+for user_without_list in highcorr_users_without_list:
+    highcorr_final_network.loc[user_without_list, :] = highcorr_final_follower_network[user_without_list]
+    
+    
+####################
+# Upload to dropbox
+####################
+# Make temporary directory
+tmp_dir = "/Users/ChrisTokita/Documents/Research/Tarnita Lab/Information Cascades/information-cascades/experimental/data/tmp/"
+os.mkdir(tmp_dir)
+
+# Write files to temporary directory
+lowcorr_final_network.to_csv(tmp_dir + 'lowcorr_final_network.csv', index = True)
+highcorr_final_network.to_csv(tmp_dir + 'highcorr_final_network.csv', index = True)
+
+# Upload to dropbox
+for file in ['lowcorr_final_network.csv', 'highcorr_final_network.csv']:
+    with open(tmp_dir + file, "rb") as f:
+        dbx.files_upload(f.read(), path = path_to_twitter_data + file, mode = dropbox.files.WriteMode.overwrite)  
+ 
+# Delete files
+shutil.rmtree(tmp_dir) 
